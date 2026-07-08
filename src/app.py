c1750492d8bbb87f8d27db9405cd125fdb3b5258
Mahir Durna <durna.m@outlook.de>
2026-07-08 22:04:01 +0200

    feat: add personalized greeting and dices route

--- src/app.py
+++ src/app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/<name>")
+def personalized_hello(name):
+    return f"Hello, {name}"
+
+@app.route("/dices")
+def dices():
+    return "Look at these beautiful dices"
