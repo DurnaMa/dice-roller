30bd635e0fec0b923e0a9f03673b2f537fcbd8bb
mdurna <durna.m@outlook.de>
2026-07-15 14:43:50 +0200

    feat: refactor app routes, implement template rendering function, and enhance HTML templates with new layout and styles

--- src/app.py
+++ src/app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template
+from typing import Any
 
 app = Flask(__name__)
 
+def wrap_template_rendering(template_name: str, **context: Any):
+    return render_template(template_name, _author="Mahir", **context)
+
 @app.route("/")
-@app.route("/<string:name>")
-def hello_world(name: str = None):
-    return render_template("hello.html", _name=name)
+def home():
+    return wrap_template_rendering("home.html")
 
 @app.route("/dices")
-def dices():
-    return render_template("dices.html")
+def handle_dices():
+    return wrap_template_rendering("dices.html")
+
+
+@app.route("/hello")
+@app.route("/hello/<string:name>")
+def hello_world(name: str = None):
+    return wrap_template_rendering("hello.html", _name=name)
